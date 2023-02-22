106fc3b197563072b3f61c942a5e36e656036251
NotPhantomX <not.real.phantomx.not+github@gmail.com>
2023-02-22 07:28:46 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:roymansoor/testvite.git main:gh-pages
+git push -f git@github.com:NotPhantomX/responsive-rating.git main:gh-pages
 
 cd -
