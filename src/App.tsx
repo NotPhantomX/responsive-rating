3680d9cedfce0bfb07dbf891fe6aa84c82934836
NotPhantomX <not.real.phantomx.not+github@gmail.com>
2023-02-22 08:08:19 -0500

    remove dist directory and add attribution to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ export default function App() {
   return (
     <div class="App">
       <Rating />
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">NotPhantomX</a>.
+      </div>
     </div>
   );
 }
